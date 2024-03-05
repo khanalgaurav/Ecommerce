0e4e6ec0b59c214a7e721fee44cc60d402c26d39
khanalgaurav <khanalgaurav96@gmail.com>
2024-03-05 21:19:59 +0545

    Login and cart ta birsexa respoonsive nav ma-- now updated

--- Ecommerce_Frontend/src/components/Navbar/Navbar.jsx
+++ Ecommerce_Frontend/src/components/Navbar/Navbar.jsx
@@ -28,10 +28,14 @@ function Navbar() {
 
         <div>
             <ul className={`menu md:flex md:relative fixed md:top-0 top-[82px] left-0 w-full bg-white md:gap-10 ${show?'block':'hidden'}`}>
-                <NavLink className={({isActive})=>`${isActive?'text-red-500':'text-black'}`} to= '/'><li  className='g border md:border-0 md:p-0 p-3 text-lg '>Shop</li></NavLink>
-                <NavLink className={({isActive})=>`${isActive?'text-red-500':'text-black'}`} to= '/mens'><li  className='g border md:border-0 md:p-0 p-3 text-lg'>Men</li></NavLink>
-                <NavLink className={({isActive})=>`${isActive?'text-red-500':'text-black'}`} to ='/womens'><li  className='g border md:border-0 md:p-0 p-3 text-lg'>Women</li></NavLink>
-                <NavLink className={({isActive})=>`${isActive?'text-red-500':'text-black'}`} to ='/kids'><li  className='g border md:border-0 md:p-0 p-3 text-lg'>Kids</li></NavLink>
+                <NavLink className={({isActive})=>`${isActive?'text-red-500':'text-black'}`} to= '/'><li  className='g text-center border md:border-0 md:p-0 p-3 text-lg '>Shop</li></NavLink>
+                <NavLink className={({isActive})=>`${isActive?'text-red-500':'text-black'}`} to= '/mens'><li  className='g text-center border md:border-0 md:p-0 p-3 text-lg'>Men</li></NavLink>
+                <NavLink className={({isActive})=>`${isActive?'text-red-500':'text-black'}`} to ='/womens'><li  className='g border text-center md:border-0 md:p-0 p-3 text-lg'>Women</li></NavLink>
+                <NavLink className={({isActive})=>`${isActive?'text-red-500':'text-black'}`} to ='/kids'><li  className='g border md:border-0 text-center md:p-0 p-3 text-lg'>Kids</li></NavLink>
+                <div className='flex justify-around py-5 border-b md:hidden'>
+                    <NavLink className={({isActive})=>`${isActive?'text-red-500':'text-black'}`} to='/login'><button className='border border-[#c2c2c2] py-2 px-9 rounded-3xl hover:bg-slate-500 hover:text-white'>Login</button></NavLink>
+                    <NavLink className={({isActive})=>`${isActive?'text-red-500':'text-black'}`} to='/cart'><img src={cartIcon} alt="Cart Icon" /></NavLink>
+                </div>
             </ul>
         </div>
 
